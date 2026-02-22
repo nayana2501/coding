deba03c010179842c01d551752d9824d84cd87a9
nayana2501 <nayanacode25@gmail.com>
2026-02-22 23:32:10 +0530

    Add files via upload

---
+++ factorial_recursion.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n){
+    if(n==0) return 1;
+    return n*factorial(n-1);
+//  int fact=1;
+//     for(int i=1;i<=n;i++){
+//         fact*=i;
+//     }
+//     return fact;
+ }
+
+int main(){
+    int n; cin>>n;
+    cout<<factorial(n);
+}
